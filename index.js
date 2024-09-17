999e2889dd3a1b8be99808c880b6f5ed5ef19d52
Narasimman Work <narasimman.work@gmail.com>
2024-09-17 21:31:56 +0530

    cros added

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const express = require("express")
-const app = express()
-const dotenv = require("dotenv").config()
-const { notFound, errorHandler } = require('./middlewares/errorHandler')
-const PORT = process.env.PORT || 4000
-const bodyParser = require("body-parser")
-const cookieParser = require('cookie-parser')
-const dbConnect = require("./config/dbConnect")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+var morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
+const PORT = process.env.PORT || 4000;
+const dbConnect = require("./config/dbConnect");
 const routers = require("./routes");
 
-dbConnect()
+dbConnect();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
 
-routers(app)
+routers(app);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running at PORT ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
+});
+
+morgan("tiny");
